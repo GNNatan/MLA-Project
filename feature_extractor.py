a465003a589d10d640c3ea377898bd00e4686304
Guido Pio Natalizio <gpnatalizio@gmail.com>
2025-10-25 12:53:41 +0200

    feat: extract features for slide 1

--- feature_extractor.py
+++ feature_extractor.py
@@ -3,8 +3,16 @@ import torch.nn as nn
 from torchvision import models, transforms
 from PIL import Image
 import numpy as np
+from tqdm import tqdm
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
+
+preprocess = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean = [0.485, 0.456, 0.406],
+                         std = [0.229, 0.224, 0.225])
+])
 
 def create_feature_extractor():
     base = models.resnet50(pretrained=True)
@@ -16,8 +24,8 @@ def create_feature_extractor():
 def extract_features(patches, model):
     features = []
     with torch.no_grad():
-        for img in patches:
-            x = Image.fromarray(img).unsqueeze(0).to(device)
+        for img in tqdm(patches):
+            x = preprocess(Image.fromarray(img)).unsqueeze(0).to(device)
             feat = model(x)
             feat = feat.squeeze().cpu().numpy()
             features.append(feat)
---
+++ main.py
@@ -0,0 +1,24 @@
+from feature_extractor import create_feature_extractor, extract_features, preprocess
+from PIL import Image
+import os
+from tqdm import tqdm
+import numpy as np
+
+if __name__ == "__main__":
+    feat_extractor = create_feature_extractor()
+    output_dir = "feats"
+    os.makedirs(output_dir, exist_ok=True)
+    patches = []
+    folders = ["tiles/1"]
+    for folder in folders:
+        for filename in tqdm(os.listdir(folder)):
+            if filename == "overview_with_tiles.png":
+                continue
+            full_path = os.path.join(folder, filename)
+            with Image.open(full_path) as img:
+                patch = np.array(img)
+                patches.append(patch)
+        save_name = folder.split("/")[1]
+        feat = extract_features(patches, feat_extractor)
+        np.save(f"{os.path.join(output_dir, save_name)}.npy", feat)
+                
